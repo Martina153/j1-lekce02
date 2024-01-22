846434cc269b7986ffdbd420e015045abd7c6d7d
Filip Jirsák <filip@jirsak.org>
2024-01-22 23:42:34 +0100

    Formátování kódu.

--- src/main/java/cz/czechitas/java/lekce02/HlavniProgram.java
+++ src/main/java/cz/czechitas/java/lekce02/HlavniProgram.java
@@ -3,15 +3,16 @@ package cz.czechitas.java.lekce02;
 import cz.czechitas.java.lekce02.engine.Turtle;
 
 public class HlavniProgram {
-    private Turtle zofka;
-    public void start() {
-        zofka = new Turtle();
+  private Turtle zofka;
 
-        //TODO tady bude kód
-    }
+  public static void main(String[] args) {
+    new HlavniProgram().start();
+  }
 
-    public static void main(String[] args) {
-        new HlavniProgram().start();
-    }
+  public void start() {
+    zofka = new Turtle();
+
+    //TODO tady bude kód
+  }
 
 }
